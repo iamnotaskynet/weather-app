b29451cc886e08affb96ee85f4023352dee2f3a9
iamnotaskynet <iamnotaskynet@gmail.com>
2019-02-23 23:32:23 +0200

    Initial commit with builded app

---
+++ precache-manifest.9cf8b91dc4f946d5c32f1af8957809b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dad094524e35e02d8465051cbb7400d",
+    "url": "/wether-app/static/media/hokusai-wave.4dad0945.jpg"
+  },
+  {
+    "revision": "6bcf09b98306f97f0660",
+    "url": "/wether-app/static/js/runtime~main.6bcf09b9.js"
+  },
+  {
+    "revision": "8e0fc1513896cd18d8a8",
+    "url": "/wether-app/static/js/main.8e0fc151.chunk.js"
+  },
+  {
+    "revision": "ce661b17c263758ce12d",
+    "url": "/wether-app/static/js/2.ce661b17.chunk.js"
+  },
+  {
+    "revision": "8e0fc1513896cd18d8a8",
+    "url": "/wether-app/static/css/main.b0e5edd0.chunk.css"
+  },
+  {
+    "revision": "ce661b17c263758ce12d",
+    "url": "/wether-app/static/css/2.d7ce36fe.chunk.css"
+  },
+  {
+    "revision": "e6d159faedd4ec3de9339f4cd439ddcf",
+    "url": "/wether-app/index.html"
+  }
+];
