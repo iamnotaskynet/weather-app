8c938f5741097be7afa6a7bf2142b87855bc7e05
iamnotaskynet <iamnotaskynet@gmail.com>
2019-02-23 23:56:48 +0200

    Updates

---
+++ precache-manifest.000514537a8e13a6e38f94a43141f110.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dad094524e35e02d8465051cbb7400d",
+    "url": "/wether-app/static/media/hokusai-wave.4dad0945.jpg"
+  },
+  {
+    "revision": "6bcf09b98306f97f0660",
+    "url": "/wether-app/static/js/runtime~main.6bcf09b9.js"
+  },
+  {
+    "revision": "d3ad93fe81b18ca0ba38",
+    "url": "/wether-app/static/js/main.d3ad93fe.chunk.js"
+  },
+  {
+    "revision": "ce661b17c263758ce12d",
+    "url": "/wether-app/static/js/2.ce661b17.chunk.js"
+  },
+  {
+    "revision": "d3ad93fe81b18ca0ba38",
+    "url": "/wether-app/static/css/main.b0e5edd0.chunk.css"
+  },
+  {
+    "revision": "ce661b17c263758ce12d",
+    "url": "/wether-app/static/css/2.d7ce36fe.chunk.css"
+  },
+  {
+    "revision": "30c9528175551f7939287d6b7d8f4cb5",
+    "url": "/wether-app/index.html"
+  }
+];
