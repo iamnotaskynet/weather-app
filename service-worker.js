464e440f3e17263a88a927f4715c900c43c63f1b
iamnotaskynet <iamnotaskynet@gmail.com>
2019-03-05 08:04:37 +0200

    'wether' to 'weather' 1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wether-app/precache-manifest.b2810ec7875cac92eb01ecfb96af5916.js"
+  "/weather-app/precache-manifest.b2810ec7875cac92eb01ecfb96af5916.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/wether-app/index.html", {
+workbox.routing.registerNavigationRoute("/weather-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
