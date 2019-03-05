40290e4aabaa261bbfd4bc0cb1fd6892baeb2e1d
iamnotaskynet <iamnotaskynet@gmail.com>
2019-03-05 08:05:55 +0200

    'wether' to 'weather' 2

--- precache-manifest.b2810ec7875cac92eb01ecfb96af5916.js
+++ precache-manifest.b2810ec7875cac92eb01ecfb96af5916.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
     "revision": "4dad094524e35e02d8465051cbb7400d",
-    "url": "/wether-app/static/media/hokusai-wave.4dad0945.jpg"
+    "url": "/weather-app/static/media/hokusai-wave.4dad0945.jpg"
   },
   {
     "revision": "6bcf09b98306f97f0660",
-    "url": "/wether-app/static/js/runtime~main.6bcf09b9.js"
+    "url": "/weather-app/static/js/runtime~main.6bcf09b9.js"
   },
   {
     "revision": "fa6ff35be85d756770c8",
-    "url": "/wether-app/static/js/main.fa6ff35b.chunk.js"
+    "url": "/weather-app/static/js/main.fa6ff35b.chunk.js"
   },
   {
     "revision": "ce661b17c263758ce12d",
-    "url": "/wether-app/static/js/2.ce661b17.chunk.js"
+    "url": "/weather-app/static/js/2.ce661b17.chunk.js"
   },
   {
     "revision": "fa6ff35be85d756770c8",
-    "url": "/wether-app/static/css/main.49ac29c6.chunk.css"
+    "url": "/weather-app/static/css/main.49ac29c6.chunk.css"
   },
   {
     "revision": "ce661b17c263758ce12d",
-    "url": "/wether-app/static/css/2.d7ce36fe.chunk.css"
+    "url": "/weather-app/static/css/2.d7ce36fe.chunk.css"
   },
   {
     "revision": "ac0c287893f8de58dce66bf4077899fc",
-    "url": "/wether-app/index.html"
+    "url": "/weather-app/index.html"
   }
 ];
